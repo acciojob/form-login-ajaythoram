15e2ffcccd3458debbd939bfe27b4a4a0ba320ab
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-04-27 15:34:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	const name1 =document.getElementsByName("fname").value;
+		const name2 =document.getElementsByName("lname").value;
+	alert(` ${name1} ${name2}`);
 
 }
